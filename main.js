a910b10ba6b7de6b98ea204cbaa7e08861e97d00
Aaryaveer-singh <86355394+Aaryaveer-singh@users.noreply.github.com>
2022-10-05 21:54:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+lw_score="";
+rw_score="";
+song1_status="";
+song2_status="";
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotposes);
+}
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+    fill("red");
+    stroke("red");
+    song1_status=Peter_pan_song.isPlaying();
+    if(lw_score>0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        Harry_potter_theme_song.stop();
+        if(song1_status==false){
+            Peter_pan_song.play();
+        }
+        document.getElementById("song_name").innerHTML="Peter Pan song playing";
+    }
+
+
+    song2_status=Harry_potter_theme_song.isPlaying();
+    if(rw_score>0.2){
+        circle(rightWrist_x,rightWrist_y,20);
+        Peter_pan_song.stop();
+        if(song2_status==false){
+            Harry_potter_theme_song.play();
+        }
+        document.getElementById("song_name").innerHTML="Harry Potter song playing";
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+leftWrist_x="";
+rightWrist_x="";
+leftWrist_y="";
+rightWrist_y="";
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+
+        lw_score=results[0].pose.keypoints[9].score;
+        rw_score=results[0].pose.keypoints[10].score;
+    }
+}
